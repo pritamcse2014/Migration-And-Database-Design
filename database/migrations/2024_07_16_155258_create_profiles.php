b62a015b07eb73fc59bdedbd18e7bbbc97e07e3c
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-07-17 11:47:27 +0600

    column-types-three

--- database/migrations/2024_07_16_155258_create_profiles.php
+++ database/migrations/2024_07_16_155258_create_profiles.php
@@ -12,14 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->float('dollar');
-            $table->geometryCollection('positions');
-            $table->geometryCollection('positions_2');
-            $table->integer('population');
-            $table->ipAddress('visitor');
-            $table->json('user_details');
-            $table->longText('blog_post');
+            $table->smallIncrements('id');
+            $table->mediumInteger('col1');
+            $table->mediumText('col2');
+            $table->smallInteger('col3');
+            $table->string('col4', 1000);
+            $table->time('col5');
+            $table->timestamp('col6');
+            $table->tinyInteger('col8');
+            $table->unsignedBigInteger('col9');
+            $table->unsignedInteger('col10');
+            $table->text('col11');
+            $table->unsignedMediumInteger('col12');
+            $table->unsignedTinyInteger('col13');
+            $table->unsignedSmallInteger('col14');
+            $table->tinyText('col15');
+            $table->timestamps();
         });
     }
 
